a6c213017fdb73d176a9bb9bf8dfa940ae837de1
fangjie24 <fangjie24@jd.com>
2021-12-01 18:45:40 +0800

    first commit

--- src/main/java/org/example/filters/JsfGatewayFilterFactory.java
+++ src/main/java/org/example/filters/JsfGatewayFilterFactory.java
@@ -1,5 +1,6 @@
 package org.example.filters;
 
+import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.jd.jsf.gd.GenericService;
 import com.jd.jsf.gd.config.ConsumerConfig;
@@ -108,5 +109,29 @@ public class JsfGatewayFilterFactory extends AbstractGatewayFilterFactory<JsfGat
         public void setUri(String uri) {
             this.uri = uri;
         }
+    }
+
+
+    public static void main(String[] args) {
+        // 注册中心实现（必须）
+        RegistryConfig jsfRegistry = new RegistryConfig();
+        jsfRegistry.setIndex("i.jsf.jd.com"); // 测试环境192.168.150.121 i.jsf.jd.com
+        LOGGER.info("实例RegistryConfig");
+        // 服务消费者连接注册中心，设置属性
+        ConsumerConfig<GenericService> consumerConfig = new ConsumerConfig<GenericService>();
+        consumerConfig.setRegistry(jsfRegistry);
+        consumerConfig.setProtocol("jsf");
+        consumerConfig.setSerialization("hessian");
+        consumerConfig.setInterfaceId("com.jd.jr.bill.service.abs.pzt.BillOpenPztService");// 这里写真实的类名
+        consumerConfig.setAlias("abs-pzt-open-api-beta");
+        consumerConfig.setGeneric(true); // 需要指定是Generic调用true
+        consumerConfig.setTimeout(400000);
+        LOGGER.info("实例ConsumerConfig");
+        // 得到泛化调用实例，此操作很重，请缓存consumerConfig或者service对象！！！！（用map或者全局变量）
+        GenericService service = consumerConfig.refer();
+        String xx = "{\"systemId\":\"pzj_jc\",\"systemToken\":\"pzj1461279052836290560\",\"requestNo\":\"R123123123\",\"draftNo\":\"230839102613220211116144800857\",\"draftAmount\":\"106\",\"companyUserId\":\"C1002595400\"}";
+        JSONObject inputParam = JSONObject.parseObject(xx);
+        Object o = service.$invoke("validateDraftSign", new String[]{"com.jd.jr.bill.domain.request.abs.pzt.PztValidateSignReq"}, new Object[]{inputParam});
+        System.out.println(o);
     }
 }
